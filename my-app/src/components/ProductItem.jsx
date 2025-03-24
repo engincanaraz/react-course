67e036a4fea1a397843417e20ba71d8f8fad27cb
engincanaraz <engincanaraz@outlook.com>
2025-03-25 01:16:37 +0300

    React İçerisinde JavaScript Kullanma

--- my-app/src/components/ProductItem.jsx
+++ my-app/src/components/ProductItem.jsx
@@ -1,14 +1,19 @@
 import "./ProductItem.css";
 
 function ProductItem() {
+  const imageUrl =
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREaXGeoatJyRmP9Aa-q4zaqgcUQim3bQHvjg&usqp=CAU";
+  const productName = "Soda";
+  const productPrice = 4;
+
   return (
     <div className="product-item">
       <div className="product-image">
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREaXGeoatJyRmP9Aa-q4zaqgcUQim3bQHvjg&usqp=CAU" alt="" />
+        <img src={imageUrl} alt="" />
       </div>
       <div className="product-info">
-        <h2>Soda</h2>
-        <span>4 TL</span>
+        <h2>{productName}</h2>
+        <span>{productPrice} TL</span>
       </div>
     </div>
   );
